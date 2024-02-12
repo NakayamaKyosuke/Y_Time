f66d6e9e4d6ad90a9638f39e9da8e41fea4a771b
NakayamaKyosuke <107970037+NakayamaKyosuke@users.noreply.github.com>
2024-02-12 22:50:51 +0900

    シーン基礎

--- GameJam_DriveAndAvoid/Scene/GameMainScene.h
+++ GameJam_DriveAndAvoid/Scene/GameMainScene.h
@@ -1,12 +1,34 @@
 #pragma once
 
+#include "SceneBase.h"
+#include "../Object/Player.h"
+#include "../Object/Enemy.h"
 //動作確認用
-class GameMainScene
+class GameMainScene : public SceneBase
 {
 private:
-	int i = 0;
+	int high_score; //ハイスコア
+	int back_ground; //背景画像
+	int barrier_image; //バリア画像
+	int mileage; //走行距離
+	int enemy_image[3]; //敵画像
+	int enemy_count[3]; //通り過ぎた敵カウント
+	Player* player; //プレイヤー
+	Enemy** enemy; //敵
 public:
 	GameMainScene();
-	~GameMainScene();
-	void Update();
+	virtual~GameMainScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eSceneType GetNowScene() const override;
+	
+private:
+	//ハイスコア読み込み処理
+	void ReadHighScore();
+	//当たり判定
+	bool IsHitCheak(player* p, Enemy* e);
 };
